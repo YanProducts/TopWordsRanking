0047fac92f1b8a90edb1786ba5f0e25ee88a2bb0
Okiyan-tokyo <tokyo_okiyan@yahoo.co.jp>
2024-03-17 15:49:05 +0900

    use lfs for big file

--- classes/error_sets/error_process.py
+++
@@ -1,7 +0,0 @@
-# エラーが生じた時の処理
-from flask import render_template
-
-class ErrorProcess:
-  @staticmethod
-  def error_view(message):
-    return render_template("error.html",message=message)
--- classes/modules/ginza.py
+++
@@ -1,8 +0,0 @@
-import spacy
-
-class Ginza:
-  # staticで行けそうだが、変数統一と今後のため一般メソッドに
-  def jpn_analyze(self,sentenses):
-    nlp=spacy.load("ja_ginza")
-    doc=nlp(sentenses)
-    return doc.sents
